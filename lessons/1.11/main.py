e8f291d5dd9f2be0f6b669f5260e91cc69b0d60a
January <nameisjanuary@gmail.com>
2024-04-03 20:29:01 +0400

    solved 1.11

--- lessons/1.11/main.py
+++ lessons/1.11/main.py
@@ -1,6 +1,14 @@
 # Перевороте строк или Polynomial
-
 # Напишите программу, которая принимает строку от пользователя и выводит ее в обратном порядке. (без использования встроенных функций)
 
 # Input                   :       word = ‘abcde’
 # Output                 :        word = ‘edcba’
+
+word = "abcde"
+
+for i in range(5, 1):
+    print(i)
+reversed_word = ""
+for i in range(1, len(word) + 1):
+    reversed_word = reversed_word + word[i * (-1)]
+print(reversed_word)
